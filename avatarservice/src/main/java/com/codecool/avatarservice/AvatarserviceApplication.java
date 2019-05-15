4c2395d676717546dbd2fe4bd9bb3f220016ed6f
myrelin <anna.iklody@gmail.com>
2019-05-15 03:40:55 +0200

    service registry implemented

--- avatarservice/src/main/java/com/codecool/avatarservice/AvatarserviceApplication.java
+++ avatarservice/src/main/java/com/codecool/avatarservice/AvatarserviceApplication.java
@@ -2,6 +2,7 @@ package com.codecool.avatarservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.PathSelectors;
@@ -14,6 +15,7 @@ import java.util.Random;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class AvatarserviceApplication {
 
     public static void main(String[] args) {
